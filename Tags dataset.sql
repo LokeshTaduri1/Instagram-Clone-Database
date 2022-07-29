35693be6000c1f875941941693dabfcb98dd8d25
Taduri Lokesh <88146458+LokeshTaduri1@users.noreply.github.com>
2022-07-29 14:33:08 +0530

    Update Tags dataset.sql

--- Tags dataset.sql
+++ Tags dataset.sql
@@ -1,2 +1,4 @@
 
-INSERT INTO tags(tag_name) VALUES ('sunset'), ('photography'), ('sunrise'), ('landscape'), ('food'), ('foodie'), ('delicious'), ('beauty'), ('stunning'), ('dreamy'), ('lol'), ('happy'), ('fun'), ('style'), ('hair'), ('fashion'), ('party'), ('concert'), ('drunk'), ('beach'), ('smile');
+INSERT INTO tags(tag_name) VALUES ('sunset'), ('photography'), ('sunrise'), ('landscape'), ('food'), ('foodie'), 
+('delicious'), ('beauty'), ('stunning'), ('dreamy'), ('lol'), ('happy'), ('fun'), ('style'), ('hair'), ('fashion'), 
+('party'), ('concert'), ('drunk'), ('beach'), ('smile');
